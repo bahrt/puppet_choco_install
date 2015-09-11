7cb798cce5693f2458bc95074e27d0ca7c1aa0ae
Sebastian Jeremias <sebastian.jeremias@thewarrantygroup.com>
2015-09-11 13:07:16 -0300

    removed choco url parameter

--- pe_choco_install.cmd
+++ pe_choco_install.cmd
@@ -1,3 +1,6 @@
+rem Run this: 
+rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/bahrt/puppet_choco_install/master/pe_choco_install.cmd'))"
+
 rem Environment variables
 set SOFTDIR=C:\software
 set PE_INSTALL_DIR=C:\tools\Puppet
@@ -9,7 +12,7 @@ set PE_FILE_NAME=puppet-agent-x64-latest.msi
 set PE_DOWNLOAD_URL=https://downloads.puppetlabs.com/windows/%PE_FILE_NAME%
 set PE_INSTALLER=%SOFTDIR%\%PE_FILE_NAME%
 rem Choco download url must be quoted for PS to treat it as a string
-set CHOCO_DOWNLOAD_URL='https://chocolatey.org/install.ps1'
+rem set CHOCO_DOWNLOAD_URL='https://chocolatey.org/install.ps1'
 
 
 rem Set execution policy to RemoteSigned for later PS1 scripts
@@ -21,5 +24,5 @@ mkdir %SOFTDIR%
 msiexec /qn /norestart /i %PE_INSTALLER% /l*v %SOFTDIR%\pe_agent_install.log INSTALLDIR=%PE_INSTALL_DIR% PUPPET_MASTER_SERVER=%PE_SERVER%
 
 rem Chocolatey installation - does not allow custom dir installation
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString(%CHOCO_DOWNLOAD_URL%))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
